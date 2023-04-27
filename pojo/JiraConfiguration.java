a351d25125a1abdf10c4fa7e82dd1b66a9ed7b45
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 15:25:24 +0530

    initial commit

---
+++ pojo/JiraConfiguration.java
@@ -0,0 +1,103 @@
+public class JiraConfiguration {
+ 
+ private String username;
+ private String password;
+ private String url;
+ private String repositoryName;
+ 
+ public String getUsername() {
+  return username;
+ }
+ 
+ public void setUsername(String username) {
+  this.username = username;
+ }
+ 
+ public String getPassword() {
+  return password;
+ }
+ 
+ public void setPassword(String password) {
+  this.password = password;
+ }
+ 
+ public String getUrl() {
+  return url;
+ }
+ 
+ public void setUrl(String url) {
+  this.url = url;
+ }
+ 
+ public String getRepositoryName() {
+  return repositoryName;
+ }
+ 
+ public void setRepositoryName(String repositoryName) {
+  this.repositoryName = repositoryName;
+ }
+ 
+ public void navigateToSacralAiWebsite(){
+  // Code to navigate to Sacral.ai website
+ }
+ 
+ public void loginIntoSacralAiWebsite(){
+  // Code to login into Sacral.ai website
+ }
+ 
+ public void accessExpertServicesToChangeBusinessPage(){
+  // Code to access Expert Services to change Business page
+ }
+ 
+ public void clickConfigure(){
+  // Code to click on Configure
+ }
+ 
+ public void clickJiraSoftware(){
+  // Code to click on Jira Software
+ }
+ 
+ public void resetForm(){
+  // Code to reset the form
+ }
+ 
+ public void saveForm(){
+  // Code to save the form
+ }
+ 
+ public boolean validateJiraSoftwareCredentials(){
+  // Code to validate the Jira Software credentials
+  
+  return true;
+ }
+ 
+ public void displayList(){
+  // Code to display the list
+ }
+ 
+ public void editListEntries(){
+  // Code to edit the list entries
+ }
+ 
+ public void deleteListEntries(){
+  // Code to delete the list entries
+ }
+ 
+ public void changeNoOfEntries(){
+  // Code to change the number of entries to display
+ }
+ 
+ public void displayPagination(){
+  // Code to display the pagination
+ }
+ 
+ public void clickAddMore(){
+  // Code to click on the Add more button
+ }
+ 
+ public void displayPopupForm(){
+  // Code to display the Pop Up form
+ }
+ 
+ 
+}
